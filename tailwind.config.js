badedca2be8b27db6a726e15811acae33e3f5858
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-03-13 15:46:44 +0100

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/**/*.{html,js}"],
+  theme: {
+    extend: {
+      //add your extensions here colors, fonts, etc..
+    },
+  },
+  plugins: [],
+}
+
