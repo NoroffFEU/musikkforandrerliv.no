ac0bd23c6d5c38d509204131af702e8ade2784be
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-03-15 13:01:21 +0100

    Added color component classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   theme: {
     extend: {
       //add your extensions here colors, fonts, etc..
+      colors: {
+        sunsetRed: '#EE6352',
+        primaryGreen: '#5b8e7d',
+        skyBlue: '#66C3FF',
+        ivoryBeige: '#F8EED1',
+        darkGreen: '#363732',
+        altRed: '#81362C',
+        forestGreen: '#304B42',
+        frostBlue: '#EBF7FF',
+      }
     },
   },
   plugins: [],
