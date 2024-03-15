f87b98af1c1522f0365272bb20b2b6e85cb293d8
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-03-15 20:29:42 +0100

    added fonts (#79)
    * added fonts
    
    * Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "montserrat": ["Montserrat", "sans-serif"],
+      "justAnotherHand": ["Just Another Hand","sans-serif"]
+    },
     extend: {
-      //add your extensions here colors, fonts, etc..
     },
   },
   plugins: [],
