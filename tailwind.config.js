25b8d9e65b6302c9611551440b1a3c4820d57da4
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-03-15 13:28:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "montserrat": ["Montserrat", "sans-serif"],
+      "justAnotherHand": ["Just Another Hand","sans-serif"]
+    },
     extend: {
-      //add your extensions here colors, fonts, etc..
-      fontFamily: {
-        "montserrat": ["Montserrat", "sans-serif"],
-        "justAnotherHand": ["Just Another Hand","sans-serif"]
-      },
     },
   },
   plugins: [],
