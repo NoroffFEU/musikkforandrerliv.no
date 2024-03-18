c173b6a54e76cb68ddf17ffcc031860513c6eca0
benjaminskarding <benjamin.skarding@hotmail.com>
2024-03-18 16:03:14 +0100

    Moved "colors" to the extend module in tailwind cfg file. It was overwriting the default colors when placed directly under "theme", making all the default colors inaccessible.
    Added "sans" keyword to fontfamily module's main font.
    - This keyword makes the browser default to that font. This way we don't have to define the default font everywhere.
    
    Changed the padding settings for the alt-button component
    - Done in order to center the text vertically. It was slightly offset when using same padding on top and bottom for some reason.
    
    Added two custom css classes in input.css
    - The first is for animating the language options.
    - The second for hiding an ugly browser default styling for cancelling user input (in search fields for example).
    
    Created HTML for header and a search drawer that will be triggered using js.
    - It was neccessary to have the search-drawer code outside the header tag.
    
    Added header code to all available html pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,22 @@
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
-    colors: {
-      'sunset-red': '#EE6352',
-      'primary-green': '#5B8E7D',
-      'sky-blue': '#66C3FF',
-      'ivory-beige': '#F8EED1',
-      'dark-green': '#363732',
-      'alt-red': '#81362C',
-      'forest-green': '#304B42',
-      'frost-blue': '#EBF7FF'
-    },
-    
     fontFamily: {
-      "montserrat": ["Montserrat", "sans-serif"],
-      "justAnotherHand": ["Just Another Hand","sans-serif"]
+      sans: ["Montserrat", "sans-serif"], /* Sans keyword sets this to default font */
+      justAnotherHand: ["Just Another Hand","sans-serif"],
     },
     
     extend: {
+      colors: {  
+        'sunset-red': '#EE6352',
+        'primary-green': '#5B8E7D',
+        'sky-blue': '#66C3FF',
+        'ivory-beige': '#F8EED1',
+        'dark-green': '#363732',
+        'alt-red': '#81362C',
+        'forest-green': '#304B42',
+        'frost-blue': '#EBF7FF'
+      },
     },
   },
   plugins: [],
