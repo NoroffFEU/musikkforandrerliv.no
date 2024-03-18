9c11a8ed2f41af62dc9249d236a2e4bf8aea03a7
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-03-18 14:48:13 +0100

    Added color component classes (#86)
    * Added color component classes
    
    * name changes in the color component
    
    ---------
    
    Co-authored-by: Jesús Alberola Herrero <127695115+Whisperpiano@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,26 @@
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
+    colors: {
+      'sunset-red': '#EE6352',
+      'primary-green': '#5B8E7D',
+      'sky-blue': '#66C3FF',
+      'ivory-beige': '#F8EED1',
+      'dark-green': '#363732',
+      'alt-red': '#81362C',
+      'forest-green': '#304B42',
+      'frost-blue': '#EBF7FF'
+    },
+    
     fontFamily: {
       "montserrat": ["Montserrat", "sans-serif"],
       "justAnotherHand": ["Just Another Hand","sans-serif"]
     },
+    
     extend: {
     },
   },
   plugins: [],
 }
+
 
