db18f8a9fc407b6dffcfdd7e67b25af187cd3125
Victor Winsjansen <victor.winsjansen@icloud.com>
2024-03-22 10:12:44 +0700

    Inserted HTML/style to section 3 – Our Work page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,29 @@
 module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
-    colors: {  
-      'sunset-red': '#EE6352',
-      'primary-green': '#5B8E7D',
-      'sky-blue': '#66C3FF',
-      'ivory-beige': '#F8EED1',
-      'dark-green': '#363732',
-      'alt-red': '#81362C',
-      'forest-green': '#304B42',
-      'frost-blue': '#EBF7FF',
-      'white': '#FFFFFF',
-      'black': '#000000'
+    colors: {
+      "sunset-red": "#EE6352",
+      "primary-green": "#5B8E7D",
+      "sky-blue": "#66C3FF",
+      "ivory-beige": "#F8EED1",
+      "dark-green": "#363732",
+      "alt-red": "#81362C",
+      "light-red": "#FCE2DB",
+      "hover-red": "#F28077",
+      "forest-green": "#304B42",
+      "frost-blue": "#EBF7FF",
+      white: "#FFFFFF",
+      black: "#000000",
     },
     fontFamily: {
-      sans: ["Montserrat", "sans-serif"], /* Sans keyword sets this to default font */
-      justAnotherHand: ["Just Another Hand","sans-serif"],
+      sans: [
+        "Montserrat",
+        "sans-serif",
+      ] /* Sans keyword sets this to default font */,
+      justAnotherHand: ["Just Another Hand", "sans-serif"],
     },
-    
-    extend: {
-    },
+
+    extend: {},
   },
   plugins: [],
-}
-
-
+};
