2c821e95e124462ff371669b4ab37faafdff9689
LauraBLei <146441897+LauraBLei@users.noreply.github.com>
2024-03-25 08:38:54 +0100

    217 fix pr problem (#218)
    * trying out navbar with position fixed
    
    * section 2
    
    Section 2 created. Needs media query and may have to change the styling on the image when we have the entiere page complement
    
    * final adjustments for navigation bar to work with absolute / fixed positioning
    
    * additional comment
    
    * Created HTML and style section 1 Our Work page
    
    * Created HTML structure with placeholders and style
    
    * Inserted HTML/style to section 3 – Our Work page
    
    * wip
    
    * created new styles and made it smaller
    
    * updated tailwind
    
    * Update index.html
    
    * Update tailwind.config.js
    
    * Fixed image path in section 1 - index.html
    
    ---------
    
    Co-authored-by: Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
    Co-authored-by: Iseost <iselin.osterhus@gmail.com>
    Co-authored-by: EmmaCaroline <emma-caroline@hotmail.com>
    Co-authored-by: Victor Winsjansen <victor.winsjansen@icloud.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/**/*.{html,js}"],
+  content: [
+    "*.html",
+    "/pages/*.html"
+  ],
   theme: {
-    colors: {  
-      'sunset-red': '#EE6352',
-      'primary-green': '#5B8E7D',
-      'sky-blue': '#66C3FF',
-      'ivory-beige': '#F8EED1',
-      'dark-green': '#363732',
-      'alt-red': '#81362C',
-      'forest-green': '#304B42',
-      'frost-blue': '#EBF7FF',
-      'white': '#FFFFFF',
-      'black': '#000000'
+    colors: {
+      "sunset-red": "#EE6352",
+      "primary-green": "#5B8E7D",
+      "sky-blue": "#66C3FF",
+      "ivory-beige": "#F8EED1",
+      "dark-green": "#363732",
+      "alt-red": "#81362C",
+      "light-red": "#FCE2DB",
+      "hover-red": "#F28077",
+      "forest-green": "#304B42",
+      "frost-blue": "#EBF7FF",
+      white: "#FFFFFF",
+      black: "#000000",
     },
     fontFamily: {
       sans: ["Montserrat", "sans-serif"], /* Sans keyword sets this to default font */
       justAnotherHand: ["Just Another Hand","sans-serif"],
+      inter: ["Inter", "sans-serif"],
     },
     
     extend: {
+      backgroundImage: {
+        "ourWorkHeroImage": "url('../assets/placeholder-images/heroImg_testing.jpg')",
+      }
     },
+
+    extend: {},
   },
   plugins: [],
-}
-
-
+};
