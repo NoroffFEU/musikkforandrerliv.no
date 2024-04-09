c2df995894bb6b49137375f4ef5f995396843163
Victor Winsjansen <105510116+vicwinsj@users.noreply.github.com>
2024-04-09 23:10:26 +0700

    308 update   section 4   home page (#358)
    * Initial commit
    
    * Updated section 4 index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "*.html",
-    "./pages/*.html"
-  ],
+  content: ["*.html", "./pages/*.html"],
   theme: {
     colors: {
       "sunset-red": "#EE6352",
@@ -14,25 +11,31 @@ module.exports = {
       "alt-red": "#81362C",
       "light-red": "#FCE2DB",
       "hover-red": "#F28077",
+      "alt-hover-red": "#be4e43",
       "forest-green": "#304B42",
+      "mint-green": "#b2cac2",
       "frost-blue": "#EBF7FF",
       white: "#FFFFFF",
       black: "#000000",
     },
     fontFamily: {
-      sans: ["Montserrat", "sans-serif"], /* Sans keyword sets this to default font */
-      justAnotherHand: ["Just Another Hand","sans-serif"],
+      sans: [
+        "Montserrat",
+        "sans-serif",
+      ] /* Sans keyword sets this to default font */,
+      justAnotherHand: ["Just Another Hand", "sans-serif"],
       inter: ["Inter", "sans-serif"],
     },
 
     extend: {
-        backgroundImage: {
-          'tompe': "url('/assets/placeholder-images/IMG_4216.webp')",
-        },
-        backgroundImage: {
-          'ourWorkHeroImage': "url('../assets/placeholder-images/heroImg_testing.jpg')",
-        },
-    plugins: [],
+      backgroundImage: {
+        tompe: "url('/assets/placeholder-images/IMG_4216.webp')",
+      },
+      backgroundImage: {
+        ourWorkHeroImage:
+          "url('../assets/placeholder-images/heroImg_testing.jpg')",
+      },
+      plugins: [],
     },
-  }
-}
+  },
+};
