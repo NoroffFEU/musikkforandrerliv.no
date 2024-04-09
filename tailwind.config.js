753620b96b4c5b6705ff29792b72a64a45120739
lem31 <112780194+lem31@users.noreply.github.com>
2024-04-09 12:50:42 +0000

    Deploying to gh-pages from @ NoroffFEU/musikkforandrerliv.no@184f2c197b822b7ab0e765f068d9de504f127155 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.html",
+    "./pages/*.html"
+  ],
+  theme: {
+    colors: {
+      "sunset-red": "#EE6352",
+      "primary-green": "#5B8E7D",
+      "sky-blue": "#66C3FF",
+      "ivory-beige": "#F8EED1",
+      "dark-green": "#363732",
+      "alt-red": "#81362C",
+      "light-red": "#FCE2DB",
+      "hover-red": "#F28077",
+      "forest-green": "#304B42",
+      "frost-blue": "#EBF7FF",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"], /* Sans keyword sets this to default font */
+      justAnotherHand: ["Just Another Hand","sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+
+    extend: {
+        backgroundImage: {
+          'tompe': "url('/assets/placeholder-images/IMG_4216.webp')",
+        },
+        backgroundImage: {
+          'ourWorkHeroImage': "url('../assets/placeholder-images/heroImg_testing.jpg')",
+        },
+    plugins: [],
+    },
+  }
+}
