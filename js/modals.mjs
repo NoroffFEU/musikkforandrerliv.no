cbceae75e6db1d7a54a1b1bc1a72f5cd592018c8
Julie <142581387+juliehabb@users.noreply.github.com>
2024-04-12 09:23:22 +0200

    Edited modal function to be more modular and added modal to donation and fundraising. (#438)

--- js/modals.mjs
+++ js/modals.mjs
@@ -1,12 +1,37 @@
 
-function toggleModal() {
-    let modal = document.querySelector("#volunteer-modal");
+
+
+function toggleModal(modalId) {
+    let modal = document.querySelector(`#${modalId}`);
     modal.classList.toggle("hidden");
 }
 
+//VOLUNTEER:
 //Opens the modal
-document.querySelector(".volunteer-form-btn").addEventListener("click", toggleModal);
+document.querySelector(".volunteer-form-btn").addEventListener("click", function() {
+    toggleModal("volunteer-modal");
+});
 
 //Hides the modal
-document.querySelector("#close-icon").addEventListener("click", toggleModal);
+document.querySelector("#volunteer-close-icon").addEventListener("click", function () {
+    toggleModal("volunteer-modal");
+});
 
+//DONATION:
+document.querySelector("#donation-form-btn").addEventListener("click", function() {
+    toggleModal("donation-modal");
+});
+
+document.querySelector("#donation-close-icon").addEventListener("click", function () {
+    console.log("close");
+    toggleModal("donation-modal");
+});
+
+//FUNDRAISING:
+document.querySelector("#fundraising-form-btn").addEventListener("click", function() {
+    toggleModal("fundraiser-modal");
+});
+
+document.querySelector("#fundraising-close-icon").addEventListener("click", function () {
+    toggleModal("fundraiser-modal");
+});
