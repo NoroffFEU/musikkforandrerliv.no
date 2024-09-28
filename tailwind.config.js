b320355adc71bcef6289d108dc05c06e292df261
cgrotteland <chrgro02122@stud.noroff.no>
2024-09-28 14:33:08 +0200

    moved bg out of extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,14 +27,11 @@ module.exports = {
       inter: ["Inter", "sans-serif"],
     },
 
-    extend: {
-      backgroundImage: {
-        tompe: "url('/assets/placeholder-images/IMG_4216.webp')",
-      },
-      backgroundImage: {
-        ourWorkHeroImage: "url('../assets/placeholder-images/IMG_3779.webp')",
-      },
-      plugins: [],
+    extend: {},
+    backgroundImage: {
+      tompe: "url('/assets/placeholder-images/IMG_4216.webp')",
+      ourWorkHeroImage: "url('../assets/placeholder-images/IMG_3779.webp')",
     },
+    plugins: [],
   },
 };
