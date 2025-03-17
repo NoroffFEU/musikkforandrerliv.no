0912e6cbf9079269a480575e1b67985baa1068bf
Odd Egil Hegge Selnes <post@odde.org>
2025-03-17 21:41:13 +0100

    installed and set-up react with centralized file and added default routes and a 404 NotFound route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import heart from '/assets/images/svgs/heart.svg';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes";
+import heart from "/assets/images/svgs/heart.svg";
 
 function App() {
-
   return (
     <>
-      <div className='h-screen w-full flex justify-center items-center'>
-        <h1 className='text-2xl font-extrabold'>Welcome to MMF project <img className='inline bg-red-600' src={heart} alt="" /></h1>
-      </div> 
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
