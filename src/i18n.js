36aae40ab98987efcc6219e29c243383a1dbf63c
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-03-18 08:53:52 +0100

    adds install and configure react-i18next

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(HttpApi) // Load translations via HTTP
+  .use(LanguageDetector) // Detects user language
+  .use(initReactI18next) // Initializes react-i18next
+  .init({
+    supportedLngs: ["en", "fr", "es"], // Supported languages
+    fallbackLng: "en", // Default language
+    debug: true,
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json", // Translation files location
+    },
+  });
+
+export default i18n;
