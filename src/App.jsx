579295da2dc6045cc9005b0a36f0415362dd5b87
Elin Thoen Jakobsen <126721502+elinkrull@users.noreply.github.com>
2025-03-18 12:07:28 +0100

    736 install and configure react i18next (#801)
    * adds install and configure react-i18next
    
    * adds en, no, mg as supported languages
    
    * Set up I18nextProvider in App.js
    
    * chg button language to NO for testing if installation and setup works fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import heart from '/assets/images/svgs/heart.svg';
+import React from "react";
+import { I18nextProvider } from "react-i18next";
+import { useTranslation } from "react-i18next"; // You already get 'i18n' from this hook
+import heart from "/assets/images/svgs/heart.svg";
 
-function App() {
+const App = () => {
+  const { t, i18n } = useTranslation(); // i18n from the hook
 
   return (
-    <>
-      <div className='h-screen w-full flex justify-center items-center'>
-        <h1 className='text-2xl font-extrabold'>Welcome to MMF project <img className='inline bg-red-600' src={heart} alt="" /></h1>
-      </div> 
-    </>
-  )
-}
+    <I18nextProvider i18n={i18n}>
+      <div className="h-screen w-full flex justify-center items-center">
+        <h1 className="text-2xl font-extrabold">
+          {t("Welcome to MMF project")}{" "}
+          <img className="inline bg-red-600" src={heart} alt="" />
+        </h1>
+        <button onClick={() => i18n.changeLanguage("no")}>NO</button>
+      </div>
+    </I18nextProvider>
+  );
+};
 
-export default App
+export default App;
