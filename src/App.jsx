a1d2f33c9ad4253327c525913a5a399d77cd9c65
VAbrishamii <vahide.abri@yahoo.com>
2025-03-18 13:15:37 +0100

    add classes for style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-
+import WorkSection from './components/homePage/workSection';
 import heart from '/assets/images/svgs/heart.svg';
 
 function App() {
@@ -7,8 +6,8 @@ function App() {
   return (
     <>
       <div className='h-screen w-full flex justify-center items-center'>
-        <h1 className='text-2xl font-extrabold'>Welcome to MMF project <img className='inline bg-red-600' src={heart} alt="" /></h1>
-
+        {/* <h1 className='text-2xl font-extrabold'>Welcome to MMF project <img className='inline bg-red-600' src={heart} alt="" /></h1> */}
+         <WorkSection />
       </div> 
     </>
   )
--- src/components/homePage/workSection.jsx
+++ src/components/homePage/workSection.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import content from '../../data/landing-page-content.json';
+import React from "react";
+import data from "../../data/landing-page-content.json";
 
 export default function WorkSection() {
-    const { image, title, description } = content.workSection;
-    console.log(content.workSection); // delete this line
-    return (
-        <section id="WorkSection">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <img src={image} alt={title} />
-                    </div>
-                    <div className="col-lg-6">
-                        <h2>{title}</h2>
-                        <p>{description}</p>
-                    </div>
-                    <div className="col-lg-12">
-                        <button className="btn">READ MORE</button>
-                    </div>    
-                </div>
-            </div>
-        </section>
-    );
+  const { image, title, content } = data.work;
+  console.log(data.work); // delete this line
+  return (
+    <section id="WorkSection" className="py-16 px-8 bg-emerald-100">
+      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-10 md:gap-16 lg:gap-20">
+       
+          <div className="w-full md:w-1/2 flex items-center justify-center">
+            <img src={image} alt={title} className="w-full aspect-[3/3] object-cover  " />
+          </div>
+
+          <div className="w-full md:w-1/2 lg:w-1/2 flex flex-col items-center justify-center md:items-start gap-6 lg:py-10">
+            <h2 className="text-3xl font-bold mb-6">{title}</h2>
+            <p className="mb-6 max-w-md">{content}</p>
+            <button
+              className=" w-[215px] h-[57px] py-[20px] px-[30px] border rounded-[9px] items-center justify-center">
+              READ MORE
+            </button>
+          </div>
+
+      </div>
+    </section>
+  );
 }
