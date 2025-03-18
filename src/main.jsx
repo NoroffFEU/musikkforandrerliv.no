ed3fc4c410046f8544f5a5a012ed7f2b29e94f1d
H-chai <143179912+H-chai@users.noreply.github.com>
2025-03-18 21:49:02 +0100

    Check HeroSection in live server

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { HeroSection } from "./components/homePage/heroSection.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HeroSection /> {/* Test HeroSection independently */}
+    {/* <App /> */}
+  </StrictMode>
+);
