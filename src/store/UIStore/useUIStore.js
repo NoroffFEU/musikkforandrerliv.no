f0256ce5c4cd5700447213ff770ec6157352869b
Haze <117860981+HansiFED@users.noreply.github.com>
2025-03-19 12:45:54 +0100

    Fix: - Added new name to sidebar state function
    - Added more js docs for clarity
    
    - Added example of usage of the function

--- src/store/UIStore/useUIStore.js
+++ src/store/UIStore/useUIStore.js
@@ -20,17 +20,33 @@ const useModalStore = create((set) => ({
 }));
 
 /**
+ * A Zustand store for managing the sidebar's open/closed state.
+ *
+ * This store provides methods to toggle, open, and close the sidebar.
+ *
+ * @example
+ * const { isSidebarOpen, toggleSidebar, openSidebar, closeSidebar } = useSidebarStore();
+ *
+ * // Open the sidebar
+ * openSidebar();
+ *
+ * // Close the sidebar
+ * closeSidebar();
+ *
+ * // Toggle the sidebar state
+ * toggleSidebar();
+ *
  * @property {boolean} isSidebarOpen - Represents whether the sidebar is open or closed.
  * @property {() => void} toggleSidebar - Toggles the sidebar between open and closed states.
  * @property {() => void} openSidebar - Opens the sidebar.
  * @property {() => void} closeSidebar - Closes the sidebar.
  */
 
-const useUIStore = create((set) => ({
+const useSidebarStore = create((set) => ({
   isSidebarOpen: false,
   toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
   openSidebar: () => set({ isSidebarOpen: true }),
   closeSidebar: () => set({ isSidebarOpen: false }),
 }));
 
-export { useModalStore, useUIStore };
+export { useModalStore, useSidebarStore };
