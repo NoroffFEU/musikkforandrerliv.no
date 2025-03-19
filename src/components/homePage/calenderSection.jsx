ed9f3957f9942c19a93926dd75360417d3808d0e
VAbrishamii <vahide.abri@yahoo.com>
2025-03-19 18:26:16 +0100

    update calenderSection style

--- src/components/homePage/calenderSection.jsx
+++ src/components/homePage/calenderSection.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import data from "../../data/landing-page-content.json";// Importing JSON file
+import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import data from "../../data/landing-page-content.json"; // Importing JSON file
 
 export default function CalenderSection() {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -9,26 +9,27 @@ export default function CalenderSection() {
 
   useEffect(() => {
     const formattedEvents = data.calender.events.map((event) => ({
-        ...event,
-        date: new Date(event.date),
-        }));
+      ...event,
+      date: new Date(event.date),
+    }));
     setEvents(formattedEvents);
-    }, []);
+  }, []);
 
-    const formatDate = (date) => date.toISOString().split('T')[0];  
+  const formatDate = (date) => date.toISOString().split("T")[0];
 
-    return (
-        <section className="w-full max-w-6xl mx-auto p-8">
-        <h2 className="text-4xl mb-6 [font-family:var(--font-justAnotherHand)]">{data.calender.title}</h2>
-  
-        {/* Event List */}
-        <div className="flex max-md:flex-col items-center ">
-        <div className="flex flex-col gap-3 max-md:hidden">
+  return (
+    <section className="w-full max-w-6xl mx-auto p-8">
+      <h2 className="text-4xl mb-6 [font-family:var(--font-justAnotherHand)]">
+        {data.calender.title}
+      </h2>
+
+      {/* Event List */}
+      <div className="w-full flex max-md:flex-col items-center">
+        <div className="flex flex-col gap-2 max-md:hidden ">
           {events.map((event, index) => (
             <div
               key={index}
-              className="bg-red-100 px-4 py-2 rounded-md shadow-sm text-lg font-medium"
-            >
+              className="bg-red-100 w-[100%] px-4 py-2 text-lg font-normal">
               {event.date.toLocaleDateString("en-US", {
                 month: "long",
                 day: "numeric",
@@ -38,23 +39,24 @@ export default function CalenderSection() {
             </div>
           ))}
         </div>
-  
+
         {/* Calendar */}
-        <div className="flex justify-center ">
-          <Calendar
+        <div className="flex justify-center">
+          <Calendar  prevLabel="<" nextLabel=">" 
             onChange={setSelectedDate}
             value={selectedDate}
-            tileClassName={({ date, view }) =>
-              view === "month" && events.some(e => formatDate(e.date) === formatDate(date))
-                ? "bg-red-200 text-red-900 rounded-md"
-                : ""
-            }
-            className="[border-color:var(--color-sunset-red)] p-4 rounded-3xl"
+            // tileClassName={({ date, view }) =>
+            //   view === "month" &&
+            //   events.some((e) => formatDate(e.date) === formatDate(date))
+            //     ? "bg-red-200 text-red-900 rounded-md"
+            //     : ""
+            // }
+            
+            
+            className=" p-4 border-1 border-red-400 rounded-3xl"
           />
         </div>
-        </div>
-      </section>
-    );
-  }
-
-  
+      </div>
+    </section>
+  );
+}
