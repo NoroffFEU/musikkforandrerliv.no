878ec234b23505cdb7a2e3ca2ecc6fc16f5807ee
Kristinebjorgan <kribjo53662@stud.noroff.no>
2025-03-20 08:24:01 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
-import heart from "/assets/images/svgs/heart.svg";
-
-function Home() {
+export function HomePage() {
   return (
-    <div className="h-screen w-full flex justify-center items-center flex-col">
-      <h1 className="text-2xl font-extrabold">
-        Welcome to MMF project
-        <img className="inline bg-red-600" src={heart} alt="" />
-      </h1>
-      <h2>Music Changes Lives</h2>
+    <div>
+      <section id="HeroSection"></section>
+      <section id="HistorySection"></section>
+      <section id="StaffSection"></section>
+      <section id="WorkSection"></section>
+      <section id="VolunteerSection"></section>
+      <section id="SupportSection"></section>
+      <section id="GallerySection"></section>
+      <section id="CalenderSection"></section>
     </div>
   );
 }
-
-export default Home;
