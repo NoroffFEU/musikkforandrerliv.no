18e472606f112245308684d29902791d21d3e79c
andgram <145982786+andgram@users.noreply.github.com>
2025-03-20 10:20:13 +0100

    Export useRequestStore from central store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,6 @@
 // Here we will export all the stores from Zustand.
 
 import { useModalStore, useSidebarStore, useDarkModeStore } from "./UIStore/useUIStore.js";
+import { useRequestStore } from "./store/APIStore/useRequestStore.js";
+
+export { useModalStore, useSidebarStore, useDarkModeStore, useRequestStore };
