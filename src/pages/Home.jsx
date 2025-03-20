7f4dac80758a35c52408743f9a57bcda1afbacf3
Mithu225 <thu@opendigital.no>
2025-03-20 14:27:57 +0100

    Adding back Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { HeroSection } from "../components/homePage/heroSection";
+
+export function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <section id="HistorySection"></section>
+      <section id="StaffSection"></section>
+      <section id="WorkSection"></section>
+      <section id="VolunteerSection"></section>
+      <section id="SupportSection"></section>
+      <section id="GallerySection"></section>
+      <section id="CalenderSection"></section>
+    </div>
+  );
+}
