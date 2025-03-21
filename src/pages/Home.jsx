92f85dbb0989e25efe3e31790a3714d519dbe47f
VAbrishamii <vahide.abri@yahoo.com>
2025-03-21 13:42:47 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import React, {lazy, Suspense} from "react";
 import { HeroSection } from "../components/homePage/heroSection";
+import WorkSection from "../components/homePage/workSection";
+
+import  VolunteerSection  from "../components/homePage/volunteerSection";
+
 import { useTranslation } from "react-i18next"; // You already get 'i18n' from this hook
 import "../i18n";
 import { Link } from "react-router-dom";
@@ -10,14 +15,12 @@ const SelectLanguageButton = () => {
     <div className="flex space-x-4">
       <button
         className="px-4 py-2 bg-blue-500 text-white rounded"
-        onClick={() => i18n.changeLanguage("en")}
-      >
+        onClick={() => i18n.changeLanguage("en")}>
         {t("english")}
       </button>
       <button
         className="px-4 py-2 bg-green-500 text-white rounded"
-        onClick={() => i18n.changeLanguage("no")}
-      >
+        onClick={() => i18n.changeLanguage("no")}>
         {t("norwegian")}
       </button>
     </div>
@@ -42,26 +45,28 @@ const Home = () => {
 
       <Link
         to="/test-translations"
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-      >
+        className="px-4 py-2 bg-blue-500 text-white rounded">
         {t("goToTestTranslations")}
       </Link>
 
       <div className="space-x-4">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={() => i18n.changeLanguage("en")}
-        >
+          onClick={() => i18n.changeLanguage("en")}>
           {t("english")}
         </button>
         <button
           className="px-4 py-2 bg-green-500 text-white rounded"
-          onClick={() => i18n.changeLanguage("no")}
-        >
+          onClick={() => i18n.changeLanguage("no")}>
           {t("norwegian")}
         </button>
       </div>
       <HeroSection />
+     
+      <WorkSection />
+      <VolunteerSection />
+     
+
       <section id="HistorySection"></section>
       <section id="StaffSection"></section>
       <section id="WorkSection"></section>
