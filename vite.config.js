83b4d58123e55aa4a5df42cddb103ca3dd8c0d1d
Abudu Abidun Sulaiman <sulaimanabiodun172@gmail.com>
2025-03-21 23:13:01 +0100

    add decap-cms and config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // server: {
+  //   fs: {
+  //     allow: ["public"]
+  //   }
+  // },
   plugins: [react()],
 });
