43ed5ef4e1312878b51a2fea45df94f433813b7f
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-24 09:58:12 +0100

    fix: fixed errorsto make sure code runs without errors
    Made changes to imports used, and commented the different sections.

--- src/components/homePage/Footer.jsx
+++ src/components/homePage/Footer.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-// import logo from "/assets/placeholder-images/logo.png";
-// import facebook from "/assets/images/svgs/facebook.svg";
+import React, { useState } from "react";
 
 function Footer() {
-  const { register, handleSubmit } = useForm();
+  const [email, setEmail] = useState("");
 
-  function onFormSubmit(data) {
-    console.log(data);
+  function onEmailSubmit(event) {
+    event.preventDefault();
+    setEmail(event.target.value);
+    console.log(event.target.value);
   }
 
   return (
     <section className="font-sans">
+      {/* Subsrcibe section */}
       <section className="h-[364px] bg-(--color-sunset-red) flex justify-center items-center">
         <div className="grid gap-8 py-20 text-center text-black max-w-[480px] m-10 sm:m-0">
           <div className="flex flex-col justify-center gap-6">
@@ -24,18 +24,14 @@ function Footer() {
 
           <form
             className="flex flex-wrap sm:flex-nowrap gap-4 justify-center"
-            onSubmit={handleSubmit(onFormSubmit)}
+            onSubmit={onEmailSubmit}
           >
             <input
               type="text"
+              value={email}
               placeholder="example@example.com"
               className="h-[49px] w-full rounded-lg border-1 bg-white p-4 sm:w-[306px] md:h-[57px]"
-              {...register("email", {
-                pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                  message: "Enter a valid email address"
-                }
-              })}
+              pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
             />
             <button className="h-[49px] md:h-[57px] w-full sm:w-[158px] bg-white text-(--color-sunset-red) rounded-lg font-semibold text-2xl active:bg-(--color-sunset-red) active:text-white hover:bg-(--color-sunset-red) hover:text-white focus:bg-(--color-sunset-red) focus:text-white focus:border focus:border-white">
               Sign Up
@@ -44,19 +40,23 @@ function Footer() {
         </div>
       </section>
 
+      {/* Links */}
       <section className="h-[1069px] bg-(--color-dark-green) text-white md:h-[553px] flex flex-col items-center relative">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 justify-center items-center mt-22 md:mt-24">
-          <div>LOGO</div>
-          {/* <img src={logo} alt="Musikk Forandrer Liv logo" className="w-[170px] h-[170px] lg:h-[250px] lg:w-[250px] col-start-1" /> */}
+          {/* Placeholder for logo */}
+          <div className="w-[170px] h-[170px] lg:h-[250px] lg:w-[250px] col-start-1">
+            LOGO
+          </div>
+
           <div className="text-2xl flex flex-col col-start-1 md:col-start-2 gap-8">
-            <Link>About Us</Link>
-            <Link>News</Link>
-            <Link>Events</Link>
+            <a>About Us</a>
+            <a>News</a>
+            <a>Events</a>
           </div>
           <div className="text-2xl flex flex-col col-start-1 md:col-start-3 gap-8">
-            <Link>Gallery</Link>
-            <Link>Our Work</Link>
-            <Link>Contact Us</Link>
+            <a>Gallery</a>
+            <a>Our Work</a>
+            <a>Contact Us</a>
           </div>
 
           <div className="col-start-2 md:col-start-4 row-start-1 mt-12 gap-4 flex flex-col">
@@ -66,15 +66,15 @@ function Footer() {
 
             <p className="uppercase text-2xl font-bold">Follow us</p>
             <div>
-              <Link>Facebook</Link>
-              <Link>Instagram</Link>
+              <a>Facebook</a>
+              <a>Instagram</a>
             </div>
           </div>
         </div>
         <div className="absolute bottom-6 flex flex-col gap-4 m-0 items-center text-center">
           <div>
-            <Link>Privacy Policy</Link>
-            <Link>Terms and Conditions</Link>
+            <a>Privacy Policy</a>
+            <a>Terms and Conditions</a>
           </div>
 
           <p>© Musikkforandrerliv</p>
