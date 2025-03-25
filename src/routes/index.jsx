c407dc08a29d2b1f40b117e7f4cf76ea39d2281e
EmmaCaroline <emma-caroline@hotmail.com>
2025-03-25 22:16:00 +0100

    add route to OurWork page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,6 +8,7 @@ import Contact from '../pages/Contact';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import TestTranslations from '../pages/TestTranslations';
+import OurWork from '../pages/OurWork';
 
 const AppRoutes = () => {
   const [loading, setLoading] = useState(false);
@@ -33,6 +34,7 @@ const AppRoutes = () => {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
+              <Route path="/ourwork" element={<OurWork />} />
               <Route path="/test-translations" element={<TestTranslations />} />
               <Route path="*" element={<NotFound />} />
             </Route>
