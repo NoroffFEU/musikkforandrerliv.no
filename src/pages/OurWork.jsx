1879c324a342cd3eabab7e591a9e044be6b349e3
EmmaCaroline <emma-caroline@hotmail.com>
2025-03-25 22:12:05 +0100

    small fix: remove a space

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -5,7 +5,7 @@ function OurWork() {
     <ErrorBoundary>
       <div className="h-screen w-full flex justify-center items-center flex-col">
         <h1 className="text-2xl font-extrabold">Our Work</h1>
-        <p> Learn about our work exploring the impact of music on lives.</p>
+        <p>Learn about our work exploring the impact of music on lives.</p>
       </div>
     </ErrorBoundary>
   );
