8f653076b56e5fe0e2e1709444b87d2fb2435a7a
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-03-26 15:48:38 +0100

    fix: comment out not found path, according to Abi's set up

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
+
 import { Route, Routes, useLocation } from 'react-router-dom';
-import MainLayout from '../components/MainLayout.jsx';
+
 import ErrorBoundary from '../components/ErrorBoundary.jsx';
 import LoadingSpinner from '../components/LoadingSpinner.jsx';
-import { useEffect, useState } from 'react';
+import MainLayout from '../components/MainLayout.jsx';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Home from '../pages/Home';
@@ -21,25 +23,24 @@ const AppRoutes = () => {
     }, 200);
 
     return () => clearTimeout(timer);
-    }, [location]);
+  }, [location]);
 
   return (
-      <ErrorBoundary>
-        {loading ? ( 
-          <LoadingSpinner /> 
-        ) : ( 
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/test-translations" element={<TestTranslations />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        )}
-      </ErrorBoundary>
-
+    <ErrorBoundary>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/test-translations" element={<TestTranslations />} />
+            {/* <Route path="*" element={<NotFound />} />*/}
+          </Route>
+        </Routes>
+      )}
+    </ErrorBoundary>
   );
 };
 
