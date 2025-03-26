f48e87e463670f9b65a08157895206b573d85bca
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2025-03-26 18:54:47 +0100

    fixed a minor border outline bug

--- src/components/buttons/reusableButton.jsx
+++ src/components/buttons/reusableButton.jsx
@@ -10,7 +10,7 @@ const ReusableButton = ({
   const baseClasses =
     'inline-block border border-black text-black font-freeman px-6 py-2 rounded cursor-pointer ' +
     'hover:bg-gray-100 hover:scale-105 transform transition duration-200 ' +
-    'focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2';
+    'focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2';
 
   // if href then renders as a <a> tag
   if (href) {
