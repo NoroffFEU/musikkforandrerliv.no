c1174db13d64b62c4ae549e285a42838e9a230e1
Whisperpiano <jesusalberola90@gmail.com>
2025-03-26 11:29:56 +0100

    fixed object position

--- src/components/ourWorkPage/heroBanner/index.jsx
+++ src/components/ourWorkPage/heroBanner/index.jsx
@@ -1,14 +1,14 @@
 function HeroBanner() {
   return (
     <section className="relative">
-      <h1 className="text-7xl font-medium [font-family:var(--font-justAnotherHand)] text-white absolute inset-0 flex items-center justify-center uppercase tracking-wider">
+      <h1 className="text-7xl font-medium [font-family:var(--font-justAnotherHand)] text-white absolute inset-0 flex items-center justify-center uppercase tracking-wider mt-52">
         Our Work
       </h1>
 
       <img
         src="/assets/placeholder-images/hero.webp"
         alt="hero banner"
-        className="w-full h-screen md:max-h-[1000px] object-cover lg:object-center object-right-bottom"
+        className="w-full h-screen md:max-h-[1000px] object-cover lg:object-center object-[83%]"
       />
     </section>
   );
