42a4dde2bf3b7a691294240c72e10c552b42a975
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-27 12:35:31 +0100

    refactor: add section ids
    add section ids for anchor tags to scroll to

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,6 @@
+import locales from '../../public/locales/locales.en.json';
 import ErrorBoundary from '../components/ErrorBoundary.jsx';
 import LatestNews from '../components/LatestNews.jsx';
-import locales from '../../public/locales/locales.en.json';
 
 function News() {
   const newsPosts = locales.screens.latestNews.posts;
@@ -8,8 +8,10 @@ function News() {
   return (
     <ErrorBoundary>
       <div className="min-h-screen container mt-64 mx-auto px-8">
-        <h1 className="text-4xl font-bold mb-8">{locales.screens.latestNews.title}</h1>
-          
+        <h1 id="latestNewsSection" className="text-4xl font-bold mb-8">
+          {locales.screens.latestNews.title}
+        </h1>
+
         <div className="px-4 py-8">
           <div className="flex flex-col gap-6">
             {newsPosts.map((post, index) => (
@@ -25,6 +27,9 @@ function News() {
               />
             ))}
           </div>
+        </div>
+        <div id="eventsSection">
+          <h2>Events Section Placeholder</h2>
         </div>
       </div>
     </ErrorBoundary>
