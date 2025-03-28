40df9ad8effcfc658431f9e5551483d2c8852768
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-03-28 12:25:23 +0100

    Revert "Merge branch '930-feat-implement-category-section' into 1044-sub-add-back-to-top-button-on-page"
    This reverts commit 9997fcf01ab43441f6cc966fefb42bdd2b25dfad, reversing
    changes made to 1452e744751ea049d793ad84cf9deb98c1501906.

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -3,7 +3,6 @@ import { Suspense } from 'react';
 import ErrorBoundary from '../components/ErrorBoundary';
 import BackToTop from '../components/buttons/BackToTop';
 import CalenderSection from '../components/homePage/calenderSection';
-import CategorySection from '../components/ourWorkPage/categorySection';
 
 function OurWork() {
   return (
@@ -11,9 +10,6 @@ function OurWork() {
       <div className="h-screen w-full flex justify-center items-center flex-col">
         <h1 className="text-2xl font-extrabold">Our Work</h1>
         <p>Learn about our work exploring the impact of music on lives.</p>
-      </div>
-      <div>
-        <CategorySection />
       </div>
       <Suspense fallback={<div>Loading...</div>}>
         <CalenderSection />
