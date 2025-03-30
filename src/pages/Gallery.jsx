b73af83085434d1fc8071ce464448292e3950ad0
Julie <julie.habbestad@hotmail.com>
2025-03-30 18:00:19 +0200

    Added images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const images = [
+  { src: '../assets/placeholder-images/IMG_1457.webp'},
+  { src: '../assets/placeholder-images/IMG_3205.webp' },
+  { src: '../assets/placeholder-images/IMG_3610.webp' },
+  { src: '../assets/placeholder-images/IMG_3634.webp'},
+  { src: '../assets/placeholder-images/IMG_3847.webp'},
+  { src: '../assets/placeholder-images/IMG_4109.webp'},
+  { src: '../assets/placeholder-images/IMG_4116.webp'},
+  { src: '../assets/placeholder-images/IMG_4202.webp' },
+  { src: '../assets/placeholder-images/IMG_5348.webp' },
+];
+
 const Gallery = () => {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center">
