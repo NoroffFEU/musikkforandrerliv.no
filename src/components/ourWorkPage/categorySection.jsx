8a10a09dab166429cdbdf988199374534b6b29c8
TheRegzi <reginekornbakk@gmail.com>
2025-03-31 14:49:09 +0200

    Added arrows on mobile view

--- src/components/ourWorkPage/categorySection.jsx
+++ src/components/ourWorkPage/categorySection.jsx
@@ -27,7 +27,16 @@ const CategorySection = () => {
                   />
                 </div>
                 <div className="md:order-2 p-8 sm:p-14 sm:mr-2">
-                  <h2 className="text-2xl md:text-2xl font-bold sm:font-semibold mb-4">{section.title}</h2>
+                  <div className="flex items-center">
+                    <h2 className="text-2xl md:text-2xl font-bold sm:font-semibold mb-4 text-center">
+                      {section.title}
+                    </h2>
+                    <img 
+                      src="/assets/placeholder-images/Arrow-forward-circle.png" 
+                      alt="Arrow to the right" 
+                      className="ml-2 w-[30px] h-[30px] md:hidden mb-4" 
+                    />
+                  </div>
                   <p className="text-black">{section.content}</p>
                 </div>
               </>
@@ -41,7 +50,16 @@ const CategorySection = () => {
                   />
                 </div>
                 <div className="md:order-1 p-8 sm:p-14 sm:mr-2">
-                  <h2 className="text-2xl md:text-2xl font-bold sm:font-semibold mb-4">{section.title}</h2>
+                  <div className="flex items-center">
+                    <h2 className="text-2xl md:text-2xl font-bold sm:font-semibold mb-4 text-center">
+                      {section.title}
+                    </h2>
+                    <img 
+                      src="/assets/placeholder-images/Arrow-forward-circle.png" 
+                      alt="Arrow to the right" 
+                      className="ml-2 w-[30px] h-[30px] md:hidden mb-4" 
+                    />
+                  </div>
                   <p className="text-gray-600">{section.content}</p>
                 </div>
               </>
