27c6269434f85869d67ef1eebfa4ff0ff35c8e0f
Anne <142426482+annechh@users.noreply.github.com>
2025-04-02 20:35:10 +0200

    Gallery carousel (#1096)
    * Feat: image carousel
    
    * Style: add flex-col to section
    
    * Style: changed some styles for the carousel
    
    * style: gradient on end images
    
    * Add endless loop
    
    * Style: removed max-w 1120. Add z-10 on buttons
    
    * Style: set fixed styles for images
    
    * Style
    
    * Style: add screen view sizes
    
    * Style: add screen view size
    
    * Adds swipe
    
    * Style: moved carousel nav buttons out one div
    
    * Comment: added back comment for temporary images
    
    * Update import name
    
    * Style: removed scale, change duration on transition, removed shadow on img
    
    * Add Lazy load to img
    
    * Add skeleton loader
    
    * Add commented out carousel with colors instead of images to see how the carousel should behave smoothly
    
    * Update path for image imports with compressed images. Large images made the carousel laggy
    
    * Add: compressed images added inside src folder
    
    * Removed commented out test carousel
    
    * Update: replaced navigation codes with useCarousel hook from carousel.jsx
    
    * Style: set transition duration-100 back to 300
    
    * Style: add div around carousel to center the carousel
    
    * Removed comment

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import ImageCarousel from '../components/galleryPage/ImageCarousel';
 import ImageGallery from '../components/galleryPage/imageGallery';
 
 const Gallery = () => {
   return (
     <section className="pt-60 pb-12 px-4 max-w-7xl mx-auto">
       <h1 className="text-4xl font-bold text-center mb-8">Gallery</h1>
+      <ImageCarousel />
       <p className="text-center mb-9">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
