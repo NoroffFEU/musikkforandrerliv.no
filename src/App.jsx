77e9bcac853248695a34512e145945b610dc3e28
Martin Kruger <martin@martinkruger.com>
2025-04-06 13:44:20 +0200

    Refactor provider to own file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { LightboxProvider } from './hooks/useLightbox';
+import { LightboxProvider } from './hooks/LightboxProvider';
 import './index.css';
 import AppRoutes from './routes';
 
