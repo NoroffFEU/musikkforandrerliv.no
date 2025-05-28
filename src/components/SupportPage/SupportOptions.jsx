72b30af6b9c63b37602d09b5d26ee2f168f4ba89
skinnyK <skinnykdev@gmail.com>
2025-05-28 15:58:17 +0200

    imported and implemented SupportOptionSignle component that takes props that define the title, text content and cta button text.

--- src/components/SupportPage/SupportOptions.jsx
+++ src/components/SupportPage/SupportOptions.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import SupportOptionSingle from './SupportOptionSingle.jsx';
 
 const SupportOptions = () => {
   return (
-    <>
+    <div className="w-full grid justify-center text-center border-2 border-pink">
       <h2>Support options</h2>
-      <div></div>
-    </>
-  )
-}
+      <div className='flex gap-2 border-2'>
+        <SupportOptionSingle title='Volunteering' textContent='' ctaText='Volunteer' />
+        <SupportOptionSingle title='Donations' textContent='' ctaText='Donate' />
+        <SupportOptionSingle title='Fundraising' textContent='' ctaText='Join Us' />
+      </div>
+    </div>
+  );
+};
 
 export default SupportOptions;
