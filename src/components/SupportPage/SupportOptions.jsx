6617e06dc200c48556bd8afd7b87b67906d81246
skinnyK <skinnykdev@gmail.com>
2025-05-28 15:36:28 +0200

    created initial setup for SupportOptions component

---
+++ src/components/SupportPage/SupportOptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SupportOptions = () => {
+  return (
+    <>
+      <h2>Support options</h2>
+      <div></div>
+    </>
+  )
+}
+
+export default SupportOptions;
