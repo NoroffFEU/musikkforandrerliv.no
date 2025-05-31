adc07979403debe4470c5ec59e20a5050481f96d
skinnyK <skinnykdev@gmail.com>
2025-05-31 13:56:34 +0200

    fixed some small styling issues that were bugging me. unnecessary transitions that made resizing the screen look a little crazy.

--- src/components/SupportPage/SupportOptionSingle.jsx
+++ src/components/SupportPage/SupportOptionSingle.jsx
@@ -9,7 +9,7 @@ const SupportOptionSingle = ({ title, ariaDescription, textContent }) => {
     <div className="w-full grid pb-4 lg:pb-16">
       {/* Mobile Accordion Toggle */}
       <div
-        className={`transition-all duration-300 relative flex items-center justify-between md:hidden ${isOpen ? 'translate-y-[-9px]' : 'translate-y-0'
+        className={`relative flex items-center justify-between md:hidden ${isOpen ? 'translate-y-[-9px]' : 'translate-y-0'
           }`}
       >
         <h3
@@ -42,7 +42,7 @@ const SupportOptionSingle = ({ title, ariaDescription, textContent }) => {
       </div>
       {/* Larger Screen size Accordion Toggle */}
       <div
-        className={`transition-all duration-300 relative hidden md:grid items-center justify-center grid-cols-[250px_auto] gap-x-4 ${isOpen ? 'translate-y-[-6px]' : 'translate-y-0 cursor-pointer'
+        className={`relative hidden md:grid items-center justify-center grid-cols-[270px_auto] gap-x-4 ${isOpen ? 'translate-y-[-6px]' : 'translate-y-0 cursor-pointer'
           }`}
       >
         <h3
@@ -58,7 +58,7 @@ const SupportOptionSingle = ({ title, ariaDescription, textContent }) => {
         >
           <span
             aria-hidden="true"
-            className={`inline-block transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${isOpen ? 'rotate-90' : 'rotate-0'
+            className={`inline-flex align-middle transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${isOpen ? 'rotate-90' : 'rotate-0'
               }`}
           >
             <svg width="32" height="32" fill="none" viewBox="0 0 32 32">
@@ -77,7 +77,7 @@ const SupportOptionSingle = ({ title, ariaDescription, textContent }) => {
       {/* Drawer / Content */}
       <div
         id="drawer"
-        className={`overflow-clip transition-all duration-300 ease-in-out max-w-[60vw] mx-auto text-left ${isOpen ? 'h-full py-4 opacity-100' : 'h-0 py-0 opacity-0'
+        className={`overflow-clip transition-[opacity,height] duration-300 ease-in-out max-w-[90vw] md:max-w-[67ch] lg:max-w-[80ch] mx-auto text-left ${isOpen ? 'h-full py-4 opacity-100' : 'h-0 py-0 opacity-0'
           }`}
         aria-hidden={!isOpen}
       >
@@ -86,7 +86,7 @@ const SupportOptionSingle = ({ title, ariaDescription, textContent }) => {
           <Link
             to="#"
             id="cta-support"
-            className="uppercase text-white bg-[var(--color-sunset-red)] py-2 px-4 rounded-[9px] my-8"
+            className="uppercase text-white font-bold bg-[var(--color-sunset-red)] py-2 px-4 rounded-[9px] my-8"
           >
             Go to Form
           </Link>
