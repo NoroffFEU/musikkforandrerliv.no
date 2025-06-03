20832a8a9391834d26ba1809c583c1a77dde941d
skinnyK <skinnykdev@gmail.com>
2025-06-03 13:36:16 +0200

    basic setup of formmodal completed - closes when clicking the close "x" and when clicking outside the modal - reusable ModalContainer exported to be used seperately if desire - implements functions from useFormModal hook to open/close/manage state and set currentForm

--- src/components/modal/FormModal.jsx
+++ src/components/modal/FormModal.jsx
@@ -1,19 +1,31 @@
-export const ModalContainer = ({ handleClose, children }) => {
+export const ModalContainer = ({ closeModal, children }) => {
   return (
-    <div className="fixed inset-0 bg-black/54 z-50" onClick={handleClose}>
-      {children}
+    <div
+      className="fixed inset-0 grid place-content-center bg-black/54 z-50"
+      onClick={closeModal}
+    >
+      <div
+        className="relative p-6 bg-white"
+        onClick={
+          (e) =>
+            e.stopPropagation() /* prevents clicking within the modal from closing it */
+        }
+      >
+        <button className="absolute top-1 right-1" onClick={closeModal}>
+          X
+        </button>
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const FormModal = ({ isModalOpen, handleClose, children }) => {
+const FormModal = ({ isModalOpen, closeModal, children }) => {
   return (
     isModalOpen && (
-      <ModalContainer>
-        {children}
-      </ModalContainer>
+      <ModalContainer closeModal={closeModal}>{children}</ModalContainer>
     )
-  )
-}
+  );
+};
 
 export default FormModal;
