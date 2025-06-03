cf5fc90128c057859aa45d9c283981ebe8f597c7
skinnyK <skinnykdev@gmail.com>
2025-06-03 13:54:45 +0200

    - created useFormModal hook - manages state currentForm that is applied to each modal - manages state if the modal is open or not - function to open the modal (openModal) - function to close the modal (closeModal)

---
+++ src/hooks/useFormModal.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useFormModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentForm, setCurrentForm] = useState(null);
+
+  const openModal = (formComponent) => {
+    setCurrentForm({ Component: formComponent });
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentForm(null);
+  };
+
+  return { isModalOpen, currentForm, openModal, closeModal };
+};
+
+export default useFormModal;
