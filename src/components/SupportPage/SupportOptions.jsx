e8f0a4588ac0c9171993cd92e8c53f826f32ea75
skinnyK <skinnykdev@gmail.com>
2025-06-03 13:39:22 +0200

    - added mapping for forms that will be added to this section. the accordion information is mapped from a json file. - added example imports for future use - created example form to guide future implementations - openModal function pulled in from useFormModal hook to attach to the button that will open the form

--- src/components/SupportPage/SupportOptions.jsx
+++ src/components/SupportPage/SupportOptions.jsx
@@ -1,16 +1,38 @@
 import supportOptionsData from '../../data/supportOptionsData.json';
 import SupportOptionSingle from './SupportOptionSingle.jsx';
 
-const SupportOptions = () => {
+// example imports until they are created:
+// import VolunteerForm from '../forms/VolunteerForm';
+// import DonationsForm from '../forms/DonationsForm';
+// import FundraisingForm from '../forms/FundraisingForm';
+
+const exampleForm = () => (
+  <form className="bg-[var(--color-mint-green)] rounded-[5px]">
+    <h2>Example Form</h2>
+    <p>No form here yet!</p>
+    {/* Add your form fields here */}
+  </form>
+);
+
+const formMap = {
+  volunteer: exampleForm, // replaced by correct form when completed
+  donations: exampleForm, // replaced by correct form when completed
+  fundraising: exampleForm, // replaced by correct form when completed
+};
+
+const SupportOptions = ({ openModal }) => {
   return (
     <section className="w-full grid justify-center text-center my-24 gap-8 bg-white">
       {supportOptionsData.map((option, index) => (
         <SupportOptionSingle
           key={option.title}
           title={option.title}
+          buttonId={option.title.toLowerCase()}
           ariaDescription={option.ariaDescription}
           textContent={option.textContent}
           tabIndex={index + 1}
+          openModal={openModal}
+          formComponent={formMap[option.title.toLowerCase()] || exampleForm}
         />
       ))}
     </section>
