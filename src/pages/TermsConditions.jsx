d36df547aa30fff8a003ef46ee9b4363517a86c2
marned91 <152280051+marned91@users.noreply.github.com>
2025-06-04 21:55:38 +0200

    Added collapsed terms section and component folder
    Add collapsible terms content sections with accessibility support.
    Individual collapsible sections with toggle functionality.
    
    Added folder to components called termsConditions

--- src/pages/TermsConditions.jsx
+++ src/pages/TermsConditions.jsx
@@ -3,11 +3,11 @@ import ErrorBoundary from '../components/ErrorBoundary';
 function TermsConditions() {
   return (
     <ErrorBoundary>
-      <div className="pt-[199px] w-full flex flex-col items-center">
+      <div className="pt-[199px] w-full flex flex-col">
         <h1 className="text-5xl font-sans text-center my-10">
           Our Terms And Conditions
         </h1>
-        <p className="text-lg font-sans w-10/12 m-4">
+        <p className="text-lg font-sans w-10/12 m-4 m-auto">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           varius enim in eros elementum tristique. Duis cursus, mi quis viverra
           ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
