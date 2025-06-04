32fafe83f6b46e02d1d8aac36d8c4bdcca6bbaaf
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-06-04 13:14:49 +0200

    feat: added terms and conditions page
    Page created, and routing added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -92,7 +92,7 @@ function Footer() {
         <div className="absolute bottom-6 flex flex-col gap-4 m-0 items-center text-center">
           <div>
             <Link to="#">{t('common.footer.privacyPolicy')}</Link>
-            <Link to="#">{t('common.footer.termsAndConditions')}</Link>
+            <Link to="/terms">{t('common.footer.termsAndConditions')}</Link>
           </div>
 
           <p>© Musikkforandrerliv</p>
