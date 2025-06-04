d89faef02f9a6323ef7873d61d588d03f71abcfa
Noor Irfan <152153984+Noor807@users.noreply.github.com>
2025-06-04 14:33:48 +0200

    Made component support form and SupportContactForm

---
+++ src/components/supportForms/TextInput.jsx
@@ -0,0 +1,19 @@
+// TextInput.jsx
+import React from 'react';
+
+const TextInput = ({ label, id, type = 'text', required = false }) => {
+  return (
+    <div>
+      <input
+        type={type}
+        id={id}
+        name={id}
+        required={required}
+        placeholder={label} // Using label as placeholder
+        className="w-full p-2 border border-black bg-gray-100 rounded-md placeholder-gray-600 focus:ring-blue-500 focus:border-blue-500"
+      />
+    </div>
+  );
+};
+
+export default TextInput;
