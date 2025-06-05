e2e43bae6b9fade6a25060fad6b8e3033e5f1321
kinseeen <kine.jakobsen@hotmail.no>
2025-06-05 11:50:37 +0200

    fix styling & add component to TermsConditionspage

--- src/components/termsConditions/GoverningLaw.jsx
+++ src/components/termsConditions/GoverningLaw.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const GoverningLaw = () => {
   return (
     <div className="w-full max-w-[85%] mx-auto px-4 font-sans py-20">
-      <h1 className="font-semibold text-[32px] leading-[120%] tracking-[0] font-montserrat">
+      <h1 className="font-semibold text-[32px] leading-[120%] tracking-[0] font-montserrat text--[#363732]">
         GOVERNING LAW
       </h1>{' '}
-      <p className="font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
+      <p className="mt-6 font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
         {' '}
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         varius enim in eros elementum tristique. Duis cursus, mi quis viverra
         ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
         Lorem ipsum dolor sit amet.{' '}
       </p>
-      <p className="font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
+      <p className="mt-6 font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
         Updated: 06/01/2024
       </p>
     </div>
