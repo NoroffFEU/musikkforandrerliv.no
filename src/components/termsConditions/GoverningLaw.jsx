fdeeb7870bf80a7f787023fb11e12292e7edf7a3
kinseeen <kine.jakobsen@hotmail.no>
2025-06-05 11:38:59 +0200

    create GoverningLaw.jsx
    added GoverningLaw component and added info.

---
+++ src/components/termsConditions/GoverningLaw.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const GoverningLaw = () => {
+  return (
+    <div className="w-full max-w-[85%] mx-auto px-4 font-sans py-20">
+      <h1 className="font-semibold text-[32px] leading-[120%] tracking-[0] font-montserrat">
+        GOVERNING LAW
+      </h1>{' '}
+      <p className="font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        Lorem ipsum dolor sit amet.{' '}
+      </p>
+      <p className="font-normal text-[18px] leading-[150%] tracking-[0] capitalize font-montserrat">
+        Updated: 06/01/2024
+      </p>
+    </div>
+  );
+};
+
+export default GoverningLaw;
