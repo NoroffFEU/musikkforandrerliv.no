09a17d5cb3c35a0dd2a72719255f266ad55ddb43
skinnyK <skinnykdev@gmail.com>
2025-06-05 08:57:47 +0200

    added hover state and cursor-pointer to button

--- src/components/SupportPage/SupportOptionSingle.jsx
+++ src/components/SupportPage/SupportOptionSingle.jsx
@@ -14,9 +14,8 @@ const SupportOptionSingle = ({
     <div className="w-full grid pb-4 lg:pb-16">
       {/* Mobile Accordion Toggle */}
       <div
-        className={`relative flex items-center justify-between md:hidden transition-discrete ${
-          isOpen ? 'translate-y-[-9px]' : 'translate-y-0'
-        }`}
+        className={`relative flex items-center justify-between md:hidden transition-discrete ${isOpen ? 'translate-y-[-9px]' : 'translate-y-0'
+          }`}
       >
         <h2
           className={`font-bold uppercase text-2xl md:text-4xl text-left  ${!isOpen && ' cursor-pointer'}`}
@@ -31,9 +30,8 @@ const SupportOptionSingle = ({
         >
           <span
             aria-hidden="true"
-            className={`inline-block transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${
-              isOpen ? 'rotate-90' : 'rotate-0'
-            }`}
+            className={`inline-block transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${isOpen ? 'rotate-90' : 'rotate-0'
+              }`}
           >
             <svg width="32" height="32" fill="none" viewBox="0 0 32 32">
               <path
@@ -49,9 +47,8 @@ const SupportOptionSingle = ({
       </div>
       {/* Larger Screen size Accordion Toggle */}
       <div
-        className={`relative hidden md:grid items-center justify-center grid-cols-[350px_auto] gap-x-4 transition-all duration-300 ${
-          isOpen ? ' translate-y-[-12px]' : ' translate-y-0 cursor-pointer'
-        }`}
+        className={`relative hidden md:grid items-center justify-center grid-cols-[350px_auto] gap-x-4 transition-all duration-300 ${isOpen ? ' translate-y-[-12px]' : ' translate-y-0 cursor-pointer'
+          }`}
       >
         <h3
           className="font-bold uppercase text-[40px] text-center"
@@ -66,9 +63,8 @@ const SupportOptionSingle = ({
         >
           <span
             aria-hidden="true"
-            className={`inline-flex align-middle transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${
-              isOpen ? 'rotate-90' : 'rotate-0'
-            }`}
+            className={`inline-flex align-middle transition-transform duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] ${isOpen ? 'rotate-90' : 'rotate-0'
+              }`}
           >
             <svg width="32" height="32" fill="none" viewBox="0 0 32 32">
               <path
@@ -86,9 +82,8 @@ const SupportOptionSingle = ({
       {/* Drawer / Content */}
       <div
         id="drawer"
-        className={`overflow-clip transition-[opacity,height] duration-300 ease-in-out max-w-[90vw] md:max-w-[67ch] lg:max-w-[80ch] mx-auto text-left ${
-          isOpen ? 'h-full py-4 opacity-100' : 'h-0 py-0 opacity-0'
-        }`}
+        className={`overflow-clip transition-[opacity,height] duration-300 ease-in-out max-w-[90vw] md:max-w-[67ch] lg:max-w-[80ch] mx-auto text-left ${isOpen ? 'h-full py-4 opacity-100' : 'h-0 py-0 opacity-0'
+          }`}
         aria-hidden={!isOpen}
       >
         <div>
@@ -106,7 +101,7 @@ const SupportOptionSingle = ({
           <button
             id={buttonId} // sets dynamic buttonId based on the title of the mapped json
             onClick={() => openModal(formComponent)} // opens modal with dynamic form component
-            className="uppercase text-white font-bold bg-[var(--color-sunset-red)] py-3 px-6 rounded-[9px] my-8"
+            className="uppercase text-white font-bold bg-[var(--color-sunset-red)] hover:bg-[var(--color-hover-red)] py-3 px-6 rounded-[9px] my-8 cursor-pointer"
           >
             Go to Form
           </button>
