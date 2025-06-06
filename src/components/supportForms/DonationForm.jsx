c53e1ea41d5f8f647037bf6dd602130941ff0a92
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-06-06 22:57:04 +0200

    refractor(donation form): add navigation to thank-you page after submission
    added navigation to thank-you page (placeholder) as required.
    #1177

--- src/components/supportForms/DonationForm.jsx
+++ src/components/supportForms/DonationForm.jsx
@@ -2,8 +2,15 @@ import ContactForm from '../SupportPage/SupportContactForm';
 import DonationRadioGroup from './DonationRadioGroup';
 
 export function DonationForm() {
+  const navigate = useNavigate();
+
+  const handleDonationSubmit = async () => {
+    await new Promise((r) => setTimeout(r, 1000));
+    navigate('/thank-you');
+  };
+
   return (
-    <ContactForm>
+    <ContactForm onSubmit={handleDonationSubmit}>
       <div className="mt-[82px] mb-16">
         <DonationRadioGroup />
       </div>
