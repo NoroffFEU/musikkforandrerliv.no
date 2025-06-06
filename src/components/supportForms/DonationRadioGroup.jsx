6397c7beab74139fc90125ff84dd02c362c24749
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-06-06 22:46:36 +0200

    refractor(donation group): refractor donation group by adding fieldsets
    added fieldsets and legend from refractored donationradioinput (radio input), for better styling and ux capabilities #1177

--- src/components/supportForms/DonationRadioGroup.jsx
+++ src/components/supportForms/DonationRadioGroup.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 
-import DonationRadioInput from './DonationRadioInput';
 
 const DonationRadioGroup = () => {
   return (
     <div>
-      <div className="flex flex-col gap-[25px] justify-center container md:flex-row md:gap-[52px] transition-all duration-300 ease-in-out">
-        <DonationRadioInput
-          legend="Choose a plan"
-          name="plan"
-          options={[
-            { label: 'One-time payment', value: 'one-time' },
-            { label: 'Every month donation', value: 'monthly' },
-          ]}
-        />
-        <DonationRadioInput
-          legend="Choose a payment method"
+      <div className="flex flex-col gap-[25px] justify-center items-center md:items-stretch container md:flex-row md:gap-[52px] transition-all duration-300 ease-in-out">
+        <fieldset  
+            aria-labelledby='Choose a plan'
+            className="min-h-[227px] h-auto flex flex-col justify-center items-center  md:items-start md:justify-start w-[280px] md:w-[505px] px-[10px] py-[35px] md:px-[70px] border-[1px] border-opacity-50 border-black/50 rounded-[9px] bg-white transition-all duration-300 ease-in-out">
+       
+          <h2 className="font-sans text-[18px] md:text-[20px] md:text-left sm:text-center text-[#363732] font-semibold mb-[44px]">
+            Choose a plan
+          </h2>
+          <RadioInput
+            name="plan"
+            options={[
+              { label: 'One-time payment', value: 'one-time' },
+              { label: 'Every month donation', value: 'monthly' },
+            ]}
+          />
+        </fieldset>
+        <fieldset  
+            aria-labelledby='Choose a payment method'
+            className="min-h-[227px] md:items-start items-center h-auto flex flex-col justify-center md:justify-start w-[280px] md:w-[505px] px-[10px] py-[35px] md:px-[70px] border-[1px] border-opacity-50 border-black/50 rounded-[9px] bg-white transition-all duration-300 ease-in-out">
+       
+          <h2 className="font-sans text-[18px] md:text-[20px] md:text-left sm:text-center text-[#363732] font-semibold mb-[44px]">
+            Choose a payment method
+          </h2>
+          <RadioInput
           name="payment"
           options={[
             { label: 'Credit Card', value: 'credit card' },
@@ -23,6 +35,7 @@ const DonationRadioGroup = () => {
             { label: 'Vipps', value: 'vipps' },
           ]}
         />
+        </fieldset>
       </div>
     </div>
   );
