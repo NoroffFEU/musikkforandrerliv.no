6bd1410aa39bc042cb70771110ff4f948fc37288
Abudu Abidun Sulaiman <sulaimanabiodun172@gmail.com>
2025-10-05 21:16:07 +0100

    add reference for useTranslation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { I18nextProvider } from "react-i18next";
+import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./i18n"; // Make sure to set up i18n for translations
 import heart from "/assets/images/svgs/heart.svg";
 import SelectLanguageButton from "./components/lang/select-language-button";
