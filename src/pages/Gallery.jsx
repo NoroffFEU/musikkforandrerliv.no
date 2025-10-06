796d4b0dcb05844652a88f31afda90fc70714f4e
Anne Ramstad <annram02249@stud.noroff.no>
2025-10-06 13:14:23 +0200

    Revert "Changed font on h1 to match figma design"
    This reverts commit 5c9aeacb158668e50348f3d5834ee5a61edd551d.

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,9 +7,7 @@ import ReadMoreSection from '../components/galleryPage/ReadMoreSection';
 const Gallery = () => {
   return (
     <section className="pt-60 pb-12 px-4 max-w-7xl mx-auto">
-      <h1 className="text-[50px] sm:text-[110px] leading-none tracking-wider font-justAnotherHand font-regular text-center mb-8">
-        Gallery
-      </h1>
+      <h1 className="text-4xl font-bold text-center mb-8">Gallery</h1>
       <ImageCarousel />
       <p className="text-center mb-9">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
