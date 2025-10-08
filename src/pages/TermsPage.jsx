516a0fa3301b9e1405035d5c8117a74b9e01d0b6
Iselin Engen <iselin.osterhus@gmail.com>
2025-10-08 23:36:37 +0200

    update

--- src/pages/TermsPage.jsx
+++ src/pages/TermsPage.jsx
@@ -14,7 +14,7 @@ const sections = [
   {
     title: 'Links to Other Websites',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit ameta.',
   },
   {
     title: 'Disclaimer of Warranties',
